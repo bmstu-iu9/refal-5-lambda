c3e89f202e47df2391ad3b803e75565992858c14
Маздайщик <a.v.konovalov87@mail.ru>
2009-05-07 11:45:00 +0300

    Commit created from recovered archive 2009-05-07_11-45-00 1-104083 031920910-differencial.rar

--- Compiler/makeself.bat
+++ Compiler/makeself.bat
@@ -4,6 +4,6 @@
 @set modules=%modules% Error.sref Generator.sref SymTable.sref
 @set modules=%modules% FindFile ParseCmdLine
 @set modules=%modules% Library LibraryEx refalrts.cpp
-@srefc_ -c "compile_cl -I..\SRLib " -d ..\SRLib %modules%
-rem @del *.obj
+@srefc_ -c "compile_bcc -I..\SRLib " -d ..\SRLib %modules%
+@del *.obj
 @del *.tds
