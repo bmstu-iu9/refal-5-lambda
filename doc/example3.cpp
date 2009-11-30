6f2f1107c96acef986d0505413ccde1f45d0182d
Маздайщик <a.v.konovalov87@mail.ru>
2009-11-30 11:55:00 +0300

    Commit created from recovered archive 2009-11-30_11-55-00 1-104939 060811966-full.rar

---
+++ doc/example3.cpp
@@ -0,0 +1,60 @@
+// Automatically generated file. Don't edit!
+#include "refalrts.h"
+
+
+//$LABEL Success
+template <typename T>
+struct SuccessL_ {
+  static const char *name() {
+    return "Success";
+  }
+};
+
+//$LABEL Fails
+template <typename T>
+struct FailsL_ {
+  static const char *name() {
+    return "Fails";
+  }
+};
+
+static refalrts::FnResult F(refalrts::Iter arg_begin, refalrts::Iter arg_end) {
+  refalrts::this_is_generated_function();
+  do {
+    refalrts::Iter bb_0 = arg_begin;
+    refalrts::Iter be_0 = arg_end;
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_left( bb_0, be_0 );
+    refalrts::move_right( bb_0, be_0 );
+    // # Success
+    if( ! refalrts::ident_left(  & SuccessL_<int>::name, bb_0, be_0 ) ) 
+      break;
+    if( ! empty_seq( bb_0, be_0 ) )
+      break;
+#ifdef INTERPRET
+    const static refalrts::ResultAction raa[] = {
+      {refalrts::icIdent, (void*) & FailsL_<int>::name},
+      {refalrts::icEnd}
+    };
+    refalrts::Iter allocs[2*sizeof(raa)/sizeof(raa[0])];
+    refalrts::FnResult res = refalrts::interpret_array( raa, allocs, arg_begin, arg_end );
+    return res;
+#else
+
+    refalrts::reset_allocator();
+    refalrts::Iter res = arg_begin;
+    refalrts::Iter n0 = 0;
+    if( ! refalrts::alloc_ident( n0, & FailsL_<int>::name ) )
+      return refalrts::cNoMemory;
+    res = refalrts::splice_elem( res, n0 );
+    refalrts::use( res );
+    refalrts::splice_to_freelist( arg_begin, arg_end );
+    return refalrts::cSuccess;
+#endif
+  } while ( 0 );
+
+  return refalrts::cRecognitionImpossible;
+}
+
+
+//End of file
