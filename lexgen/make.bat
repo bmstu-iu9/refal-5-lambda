330f813c5a89a6568be54ab20c3adeededa5ec51
Mazdaywik <a.v.konovalov87@mail.ru>
2015-04-16 21:21:37 +0300

    Исправлена утилита srmake, использование srmake в скриптах сборки

--- lexgen/make.bat
+++ lexgen/make.bat
@@ -1,8 +1,7 @@
 @echo off
 setlocal
   call ..\c-plus-plus.conf.bat
-  set modules=LexGen Generator Library LibraryEx refalrts
-  ..\bin\srefc -c "%CPPLINE% -I../srlib -DDONT_PRINT_STATISTICS" -d ..\srlib %modules%
+  ..\bin\srmake -s ..\bin\srefc -c "%CPPLINE% -I../srlib -DDONT_PRINT_STATISTICS" -d ..\srlib LexGen
   if exist a.exe move a.exe LexGen.exe
   copy LexGen.exe ..\bin\lexgen.exe
   if exist *.obj erase *.obj
--- lexgen/make.sh
+++ lexgen/make.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export GCCLINE='g++ -I../srlib -DDONT_PRINT_STATISTICS -o lexgen'
-export MODULES='LexGen Generator Library LibraryEx refalrts'
-../bin/srefc -c "$GCCLINE" -d ../srlib $MODULES
+../bin/srmake -s ../bin/srefc -c "$GCCLINE" -d ../srlib LexGen
 cp lexgen ../bin/lexgen
 rm *.cpp
