fd7af912cab8406a83d2294983c3d5dcd5c5181c
Маздайщик <a.v.konovalov87@mail.ru>
2015-06-07 14:06:11 +0300

    Исправлен сценарий сборки для unix-like

--- srmake/make.sh
+++ srmake/make.sh
@@ -2,6 +2,6 @@
 export MODULES='SRMake FileScanner FindFile ParseCmdLine
   Library LibraryEx refalrts'
 export GCCLINE='g++ -I../srlib -osrmake -DDONT_PRINT_STATISTICS'
-../bin/srefc -c "$GCCLINE" -d ../srlib -d ../Compiler $MODULES
+../bin/srefc -c "$GCCLINE" -d ../srlib -d ../compiler $MODULES
 cp srmake ../bin
 rm *.cpp
