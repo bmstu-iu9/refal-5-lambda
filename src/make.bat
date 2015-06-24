7229728b0e0124c58302a9864f2bddcb77be5db5
Маздайщик <a.v.konovalov87@mail.ru>
2015-06-24 17:46:15 +0300

    Переписан скрипт сборки
    Во-первых, теперь перед компиляцией в RSL-файлы вызывается конвертор
    от предыдущей итерации (если есть) для проверки синтаксических ошибок.
    Так сделано с двумя целями: проверка на синтаксические ошибки исходного
    текста и тестирование восстановления парсера после синтаксических
    ошибок.
    
    Диагностика синтаксических ошибок компилятора refc неудобная, поскольку
    во-первых, её не поддерживает Vim, а значит не подсвечивает строки с ошибками
    (поддержку внедрять муторно), во-вторых, при восстановлении после некоторых
    ошибок (необъявленная переменная в результате) у него начинаются ложные
    срабатывания, в-третьих, для вызовов неопределённых функций он показывает
    на конец файла, а не на сам вызов. По первому пункту — вывод ошибок изначально
    писался в стандартном виде: файл:строка:столбец:сообщение — такой формат
    поддерживается большинством IDE. По второму — каскад ошибок после первой
    возможен, но семантическая ошибка (необъявленная переменная) не может влиять
    на синтаксический анализ, т.к. тот выполняется раньше. По третьему —
    данный тип ошибок диагностируется правильно.
    
    Парсер до этого вызывался только после успешной компиляции refc, и таким
    образом, всегда на входе получал корректный исходный текст, следовательно,
    ошибки в восстановлении после ошибок остались невыявленными.
    
    Во-вторых, поскольку теперь сгерерированные файлы содержат корректный код
    на Рефале (ранее Plainer.ref работал как заглушка), то для выявления ошибок
    генерации некорректного кода добавлена компиляция выходных файлов компилятором
    refc (можно, конечно, и разрабатываемым конвертором, но достаточно и refc).

--- src/make.bat
+++ src/make.bat
@@ -1,27 +1,39 @@
 @echo off
 set RSLS=Main+Utils+Lexer+Parser+Transformer+Plainer
 set SOURCES=%RSLS:+=.ref %.ref
-for %%s in (%SOURCES%) do (
-  refc %%s >NUL 2>%temp%\stderr.txt
+if exist ..\rsls\Main.rsl call :TEST NUL
+for %%s in (%SOURCES%) do call :REFC %%s
+mkdir ..\rsls >NUL 2>NUL
+move *.rsl ..\rsls
+echo.
+
+call :TEST REFC-OUT
+
+goto :EOF
+
+:TEST
+  for %%s in (%SOURCES%) do call :PREPROCESS %%s %1
+goto :EOF
+
+:PREPROCESS
+  set SOURCE=%1
+  set TARGET=%SOURCE:.ref=.out.ref%
+  if {%2}=={NUL} set TARGET=NUL
+  echo Y | refgo ..\rsls(%RSLS%) %SOURCE% %TARGET% || exit
+  echo.
+  if {%2}=={REFC-OUT} (
+    call :REFC %TARGET%
+    erase %TARGET:.ref=.rsl%
+  )
+goto :EOF
+
+:REFC
+  refc %1 >NUL 2>%temp%\stderr.txt
   if errorlevel 1 (
-    echo %%s:
+    echo %1:
     type %temp%\stderr.txt
     erase %temp%\stderr.txt
     exit
   )
   erase %temp%\stderr.txt
-)
-mkdir ..\rsls >NUL 2>NUL
-move *.rsl ..\rsls
-echo.
-
-for %%s in (%SOURCES%) do call :test %%s
-
-goto :EOF
-
-:test
-  set SOURCE=%1
-  set TARGET=%SOURCE:.ref=.out.ref%
-  echo Y | refgo ..\rsls(%RSLS%) %SOURCE% %TARGET% || exit
-  echo.
 goto :EOF
