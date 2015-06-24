233e62fbe8c71da4b137930f74b9d55deb3f90ad
Маздайщик <a.v.konovalov87@mail.ru>
2015-06-24 18:33:35 +0300

    Заготовка трансформера
    Написана заготовка трансформера, а также доработан скрипт сборки.

--- src/make.bat
+++ src/make.bat
@@ -11,14 +11,14 @@ call :TEST REFC-OUT
 goto :EOF
 
 :TEST
-  for %%s in (%SOURCES%) do call :PREPROCESS %%s %1
+  for %%s in (%SOURCES%) do call :TRANSFORM %%s %1
 goto :EOF
 
-:PREPROCESS
+:TRANSFORM
   set SOURCE=%1
   set TARGET=%SOURCE:.ref=.out.ref%
   if {%2}=={NUL} set TARGET=NUL
-  echo Y | refgo ..\rsls(%RSLS%) %SOURCE% %TARGET% || call :FAIL
+  echo Y | refgo ..\rsls(%RSLS%) %SOURCE% %TARGET% || call :FAILED
   echo.
   if {%2}=={REFC-OUT} (
     call :REFC %TARGET%
@@ -26,7 +26,7 @@ goto :EOF
   )
 goto :EOF
 
-:FAIL
+:FAILED
   erase ..\rsls\Main.rsl
   exit
 goto :EOF
