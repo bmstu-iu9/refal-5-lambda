6bf05a8982f67deed963ca12b4dc0dd9bd053f00
Mazdaywik <a.v.konovalov87@mail.ru>
2015-09-26 11:35:51 +0300

    Доработка скриптов сборки
    Скрипт bootstrap/make.bat некорректно работал с компилятором g++.
    В скрипте c-plus-plus.conf.bat нет строки, раскомментированной
    по умолчанию.

--- bootstrap/make.bat
+++ bootstrap/make.bat
@@ -2,9 +2,15 @@
 setlocal
   mkdir ..\bin >NUL 2>NUL
   call ..\c-plus-plus.conf.bat
-  %CPPLINE% -I..\srlib srefc.cpp *.cpp
+  set FILELIST=srefc.cpp
+  for %%c in (*.cpp) do call :ADD_FILE_TO_LIST %%c
+  %CPPLINE% -I..\srlib %FILELIST%
   if exist srefc.exe move srefc.exe ..\bin
   if exist a.exe move a.exe ..\bin\srefc.exe
   if exist *.obj erase *.obj
   if exist *.tds erase *.tds
+
+  :ADD_FILE_TO_LIST
+    if not {%1}=={srefc.cpp} set FILELIST=%FILELIST% %1
+  goto :EOF
 endlocal
