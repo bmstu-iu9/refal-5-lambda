03ed51d5370365dae08d732bdd857edb467c5e6a
Mazdaywik <a.v.konovalov87@mail.ru>
2015-09-28 00:56:14 +0300

    Поддержка автоматических тестов.
    Программа тестируется как на специальных тестовых примерах,
    так и на своих собственных исходных текстах.

--- src/make.bat
+++ src/make.bat
@@ -1,16 +1,27 @@
 @echo off
-set RSLS=Main+Utils+Lexer+Parser+Transformer+Plainer
+set RSLS=Main+Utils+Lexer+Parser+Transformer+Plainer+Tests
+set RSLDIR=..\rsls
 set SOURCES=%RSLS:+=.ref %.ref
-if exist ..\rsls\Main.rsl call :TEST NUL
-for %%s in (%SOURCES%) do call :REFC %%s
-mkdir ..\rsls >NUL 2>NUL
-move *.rsl ..\rsls >NUL
 
+mkdir ..\rsls >NUL 2>NUL
+mkdir ..\rsls2 >NUL 2>NUL
+
+if exist ..\rsls\Main.rsl call :TEST NUL
+
+for %%s in (%SOURCES%) do call :REFC %%s
+move *.rsl %RSLDIR% >NUL
+
+call :TEST REFC-OUT
+
+echo ===TEST OUT===
+set RSLDIR=..\rsls2
 call :TEST REFC-OUT
 
 goto :EOF
 
 :TEST
+  call :RUN_TRANSFORMER _tests_
+  echo.
   for %%s in (%SOURCES%) do call :TRANSFORM %%s %1
 goto :EOF
 
@@ -18,15 +29,22 @@ goto :EOF
   set SOURCE=%1
   set TARGET=%SOURCE:.ref=.out.ref%
   if {%2}=={NUL} set TARGET=NUL
-  echo Y | refgo ..\rsls(%RSLS%) %SOURCE% %TARGET% || call :FAILED
+  call :RUN_TRANSFORMER %SOURCE% %TARGET%
   echo.
   if {%2}=={REFC-OUT} (
     call :REFC %TARGET%
-    erase %TARGET:.ref=.rsl%
+    move %TARGET:.ref=.rsl% ..\rsls2\%TARGET:.out.ref=.rsl% >NUL
   )
 goto :EOF
 
+:RUN_TRANSFORMER
+  echo Y | refgo %RSLDIR%(%RSLS%) %* 2>__err.txt || call :FAILED
+  erase __err.txt
+goto :EOF
+
 :FAILED
+  echo FAILED
+  type __err.txt
   erase ..\rsls\Main.rsl
   exit
 goto :EOF
@@ -37,6 +55,8 @@ goto :EOF
     echo %1:
     type %temp%\stderr.txt
     erase %temp%\stderr.txt
+    set RSLDIR=..\rsls
+    call :RUN_TRANSFORMER %1 NUL
     exit
   )
   erase %temp%\stderr.txt
