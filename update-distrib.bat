75c5f8182ec95e7b69d6d5787a2e1639c6546c03
Маздайщик <a.v.konovalov87@mail.ru>
2016-02-07 11:53:13 +0300

    Созданы скрипты обновления дистрибутива (#9)

---
+++ update-distrib.bat
@@ -0,0 +1,23 @@
+@echo off
+pushd distrib
+rd /q /s bin compiler doc lexgen srlib srmake
+xcopy /e /y ..\build\*
+md bin
+copy ..\src\scripts\srefc.* bin
+copy ..\src\scripts\srmake.* bin
+ren bin\*.sh *.
+xcopy /e /i /y ..\srlib srlib
+md doc
+xcopy /e /i /y ..\doc\examples doc\examples
+copy ..\doc\*.pdf doc
+copy ..\doc\*.jpg doc
+md doc\historical
+copy ..\doc\historical\*.txt doc\historical
+copy ..\doc\historical\*.pdf doc\historical
+copy ..\doc\historical\*.jpg doc\historical
+copy ..\doc\historical\*.doc doc\historical
+md doc\historical\Дрогунов
+copy ..\doc\historical\Дрогунов\*.pdf doc\historical\Дрогунов
+copy ..\LICENSE .
+copy ..\README.md .
+popd
---
+++ update-distrib.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pushd distrib
+rm -rf bin compiler doc lexgen srlib srmake
+cp -R ../build/* .
+mkdir bin
+cp ../src/scripts/srefc.bat bin
+cp ../src/scripts/srefc.sh bin/srefc
+cp ../src/scripts/srmake.bat bin
+cp ../src/scripts/srmake.sh bin/srmake
+cp -R ../srlib srlib
+mkdir doc
+cp -R ../doc/examples doc/examples
+cp ../doc/*.pdf doc
+cp ../doc/*.jpg doc
+mkdir doc/historical
+cp ../doc/historical/*.txt doc/historical
+cp ../doc/historical/*.pdf doc/historical
+cp ../doc/historical/*.jpg doc/historical
+cp ../doc/historical/*.doc doc/historical
+mkdir doc/historical/Дрогунов
+cp ../doc/historical/Дрогунов/*.pdf doc/historical/Дрогунов
+cp ../LICENSE .
+cp ../README.md .
+popd
