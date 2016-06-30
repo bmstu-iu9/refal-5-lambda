44f5c25d0c04c3ad0b72fd9c4395341e9fbf9f9d
Иван Скрыпников <dead_mapos@mail.ru>
2016-06-30 13:07:12 +0300

    Последние изменения

--- c-plus-plus.conf.bat
+++ c-plus-plus.conf.bat
@@ -1,4 +1,4 @@
-@echo off
+﻿@echo off
 
 rem Если переменная среды уже установлена явно, то содержимое файла игнорируется
 if not "%CPPLINE%" == "" goto :EOF
@@ -9,9 +9,9 @@ rem компилятора C++
 rem Если вы используете компилятор Visual C++, раскомментируйте одну из строчек
 rem ниже. Убедитесь, что в ней задан правильный путь к файлу vcvarsall.bat
 :: call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+:: call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 rem Не забудьте раскомментировать и эту строку
-set CPPLINE=cl /EHcs /W3 /wd4996
+:: set CPPLINE=cl /EHcs /W3 /wd4996
 
 rem Если вы используете компилятор Borland C++ Compiler 5.5
 :: set PATH=%PATH%;C:\Borland\BCC55\bin
