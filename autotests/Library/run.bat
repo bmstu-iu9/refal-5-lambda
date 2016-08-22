d5501b4d0e1b5b9cfc909950370c370d8d054601
Маздайщик <a.v.konovalov87@mail.ru>
2016-08-22 08:17:10 +0300

    Модульные тесты для библиотеки Library (#10)

--- autotests/Library/run.bat
+++ autotests/Library/run.bat
@@ -1,1 +1,227 @@
 @echo off
+call :MAIN %* || exit /b 1
+goto :EOF
+
+:MAIN
+setlocal
+  call ..\..\c-plus-plus.conf.bat
+  if {%1}=={} (
+    call :RUN_ALL_TESTS *.sref
+  ) else (
+    call :RUN_ALL_TESTS %*
+  )
+endlocal
+goto :EOF
+
+:RUN_ALL_TESTS
+setlocal
+  set TEST_CPP_FLAGS= ^
+    -I../../src/srlib ^
+    -DSTEP_LIMIT=1000 ^
+    -DMEMORY_LIMIT=1000 ^
+    -DDUMP_FILE=\"__dump.txt\" ^
+    -DDONT_PRINT_STATISTICS
+
+  copy ..\..\src\srlib\Library.sref .
+  for %%s in (Library.sref %*) do call :COMPILE %%s || exit /b 1
+
+  call :SIMPLE_TESTS OK ^
+    Library-Math-OK ^
+    Library-FOpen-FReadLine-FClose ^
+    Library-ExistFile ^
+    Library-IntFromStr-StrFromInt-Chr-Ord ^
+    Library-SymbCompare ^
+    Library-SymbType ^
+    || exit /b 1
+
+  call :SIMPLE_TESTS FAIL ^
+    Library-Math-Div-Fail ^
+    Library-Math-Mod-Fail ^
+    Library-FOpen-Fail ^
+    Library-SymbCompare-Fail ^
+    Library-SymbType-Fail ^
+    || exit /b 1
+
+  if exist Library-WriteLine.exe (
+    echo Pass Library-WriteLine test...
+    call :RUN_EXE Library-WriteLine || exit /b 1
+    call :COMPARE __out.txt 2lines.txt || exit /b 1
+    call :CLEANUP Library-WriteLine
+  )
+
+  if exist Library-WriteLine-Expr.exe (
+    echo Pass Library-WriteLine-Expr test...
+    call :RUN_EXE Library-WriteLine-Expr || exit /b 1
+    call :COMPARE __out.txt WriteLine-Expr.txt || exit /b 1
+    call :CLEANUP Library-WriteLine-Expr
+  )
+
+  if exist Library-FOpen-FWriteLine-FClose.exe (
+    echo Pass Library-FOpen-FWriteLine-FClose test...
+    call :RUN_EXE Library-FOpen-FWriteLine-FClose || exit /b 1
+    call :COMPARE __written_file.txt 2lines.txt || exit /b 1
+    call :CLEANUP Library-FOpen-FWriteLine-FClose
+  )
+
+  if exist Library-ReadLine-2lines.exe (
+    echo Pass Library-ReadLine-2lines test...
+    Library-ReadLine-2lines.exe < 2lines.txt
+    if errorlevel 1 (
+      echo TEST FAILED, SEE __dump.txt
+      exit /b 1
+    )
+    call :CLEANUP Library-ReadLine-2lines
+  )
+
+  if exist Library-ReadLine-2lines-no-eol.exe (
+    echo Pass Library-ReadLine-2lines-no-eol test...
+    Library-ReadLine-2lines-no-eol.exe < 2lines-no-eol.txt
+    if errorlevel 1 (
+      echo TEST FAILED, SEE __dump.txt
+      exit /b 1
+    )
+    call :CLEANUP Library-ReadLine-2lines-no-eol
+  )
+
+  if exist Library-Arg.exe (
+    echo Pass Library-Arg test...
+    Library-Arg.exe Hello "Hello, World"
+    if errorlevel 1 (
+      echo TEST FAILED, SEE __dump.txt
+      exit /b 1
+    )
+    call :CLEANUP Library-Arg
+  )
+
+  if exist Library-GetEnv.exe (
+    echo Pass Library-GetEnv test...
+    setlocal
+    set Foo=Bar
+    set NoEnv=
+    call :RUN_EXE Library-GetEnv || exit /b 1
+    endlocal
+    call :CLEANUP Library-GetEnv
+  )
+
+  if exist Library-Exit.exe (
+    echo Pass Library-Exit test...
+    Library-Exit.exe
+    if errorlevel 43 (
+      echo TEST FAILED, ERRCODE GREATER THAN 42
+      exit /b 1
+    ) else if not errorlevel 42 (
+      echo TEST FAILED, ERRCODE LESS THAN 42
+      exit /b 1
+    )
+    call :CLEANUP Library-Exit
+  )
+
+  if exist Library-System.exe (
+    echo Pass Library-System test...
+    call :RUN_EXE Library-System || exit /b 1
+    call :COMPARE __out.txt 2lines.txt || exit /b 1
+    call :CLEANUP Library-System
+  )
+
+  erase Library.cpp Library.sref
+endlocal
+goto :EOF
+
+:COMPILE
+setlocal
+  echo Compiling %1
+  set SRC=%1
+  set CPP=%~n1.cpp
+  set TARGET=%~n1.exe
+
+  ..\..\bin\srefc-core %SRC% 2>__error.txt
+  if errorlevel 100 (
+    echo COMPILER FAILS ON %SRC%, SEE __error.txt
+    exit /b 1
+  )
+  erase __error.txt
+  if not exist %CPP% (
+    echo COMPILATION FAILED
+    exit /b 1
+  )
+
+  if %SRC%==Library.sref (
+    endlocal
+    goto :EOF
+  )
+
+  %CPPLINE% %TEST_CPP_FLAGS% %CPP% Library.cpp ../../src/srlib/refalrts.cpp
+  if errorlevel 1 (
+    echo COMPILATION FAILED
+    exit /b 1
+  )
+  if exist a.exe move a.exe %TARGET%
+  if not exist %TARGET% (
+    echo COMPILATION FAILED
+    exit /b 1
+  )
+
+  if exist *.obj erase *.obj
+  if exist *.tds erase *.tds
+endlocal
+goto :EOF
+
+:SIMPLE_TESTS
+setlocal
+  set MODE=%1
+  shift
+  goto :%MODE%_TESTS
+
+:OK_TESTS
+  if {%1}=={} goto SIMPLE_TESTS_END
+  if exist %1.exe (
+    echo Pass simple OK test %1...
+    call :RUN_EXE %1 || exit /b 1
+    call :CLEANUP %1
+  )
+  shift
+  goto :OK_TESTS
+
+:FAIL_TESTS
+  if {%1}=={} goto SIMPLE_TESTS_END
+  if exist %1.exe (
+    echo Pass simple FAIL test %1...
+    %1.exe
+    if not errorlevel 1 (
+      echo TEST NOT EXPECTATIVE FAILED, SEE __dump.txt
+      exit /b 1
+    )
+    echo Ok! This failure was normal and expected
+    call :CLEANUP %1
+  )
+  shift
+  goto :FAIL_TESTS
+
+:SIMPLE_TESTS_END
+endlocal
+goto :EOF
+
+:RUN_EXE
+  %1 > __out.txt
+  if errorlevel 1 (
+    echo TEST FAILED, SEE __dump.txt
+    exit /b 1
+  )
+goto :EOF
+
+:CLEANUP
+  if exist %1.cpp erase %1.cpp
+  if exist %1.exe erase %1.exe
+  if exist __dump.txt erase __dump.txt
+  if exist __out.txt erase __out.txt
+  if exist __written_file.txt erase __written_file.txt
+goto :EOF
+
+:COMPARE
+  fc /b "%1" "%2"
+  if errorlevel 1 (
+    echo FILES %1 and %2 is differ:
+    fc "%1" "%2"
+    exit /b 1
+  )
+goto :EOF
--- autotests/Library/run.sh
+++ autotests/Library/run.sh
@@ -1,1 +1,198 @@
 #!/bin/bash
+source ../../c-plus-plus.conf.sh
+
+run_all_tests() {
+  TEST_CPP_FLAGS="
+    -I../../srlib \
+    -DSTEP_LIMIT=1000 \
+    -DMEMORY_LIMIT=1000 \
+    -DDUMP_FILE=\"__dump.txt\"\
+    -DDONT_PRINT_STATISTICS \
+    -g"
+
+  cp ../../src/srlib/Library.sref .
+  for s in Library.sref $*; do
+    compile $s
+  done
+
+  simple_tests ok \
+    Library-Math-OK \
+    Library-FOpen-FReadLine-FClose \
+    Library-ExistFile \
+    Library-IntFromStr-StrFromInt-Chr-Ord \
+    Library-SymbCompare \
+    Library-SymbType
+
+  simple_tests fail \
+    Library-Math-Div-Fail \
+    Library-Math-Mod-Fail \
+    Library-FOpen-Fail \
+    Library-SymbCompare-Fail \
+    Library-SymbType-Fail
+
+  if [ -e Library-WriteLine ]; then
+    echo Pass Library-WriteLine test...
+    run_exe Library-WriteLine
+    compare __out.txt 2lines.txt
+    cleanup Library-WriteLine
+  fi
+
+  if [ -e Library-WriteLine-Expr ]; then
+    echo Pass Library-WriteLine-Expr test...
+    run_exe Library-WriteLine-Expr
+    compare __out.txt WriteLine-Expr.txt
+    cleanup Library-WriteLine-Expr
+  fi
+
+  if [ -e Library-FOpen-FWriteLine-FClose ]; then
+    echo Pass Library-FOpen-FWriteLine-FClose test...
+    run_exe Library-FOpen-FWriteLine-FClose
+    compare __written_file.txt 2lines.txt
+    cleanup Library-FOpen-FWriteLine-FClose
+  fi
+
+  if [ -e Library-ReadLine-2lines ]; then
+    echo Pass Library-ReadLine-2lines test...
+    ./Library-ReadLine-2lines < 2lines.txt
+    if [ $? -gt 0 ]; then
+      echo TEST FAILED, SEE __dump.txt
+      exit 1
+    fi
+    cleanup Library-ReadLine-2lines
+  fi
+
+  if [ -e Library-ReadLine-2lines-no-eol ]; then
+    echo Pass Library-ReadLine-2lines-no-eol test...
+    ./Library-ReadLine-2lines-no-eol < 2lines-no-eol.txt
+    if [ $? -gt 0 ]; then
+      echo TEST FAILED, SEE __dump.txt
+      exit 1
+    fi
+    cleanup Library-ReadLine-2lines-no-eol
+  fi
+
+  if [ -e Library-Arg ]; then
+    echo Pass Library-Arg test...
+    ./Library-Arg Hello "Hello, World"
+    if [ $? -gt 0 ]; then
+      echo TEST FAILED, SEE __dump.txt
+      exit
+    fi
+    cleanup Library-Arg
+  fi
+
+  if [ -e Library-GetEnv ]; then
+    echo Pass Library-GetEnv test...
+    (
+      export Foo=Bar
+      export NoEnv=
+      run_exe Library-GetEnv
+    )
+    if [ $? -gt 0 ]; then
+      exit 1
+    else
+      cleanup Library-GetEnv
+    fi
+  fi
+
+  if [ -e Library-Exit ]; then
+    echo Pass Library-Exit test...
+    ./Library-Exit
+    if [ $? -ne 42 ]; then
+      echo "TEST FAILED, ERROR CODE NOT EQUAL 42 ($? != 42)"
+    fi
+    cleanup Library-Exit
+  fi
+
+  if [ -e Library-System ]; then
+    echo Pass Library-System test...
+    run_exe Library-System
+    compare __out.txt 2lines.txt
+    cleanup Library-System
+  fi
+
+  rm Library.cpp Library.sref
+}
+
+compile() {
+  echo Compiling $1
+  SRC=$1
+  CPP=${SRC%%.sref}.cpp
+  TARGET=${SRC%%.sref}
+
+  ../../bin/srefc-core $SRC 2>__error.txt
+  if [ $? -ge 100 ]; then
+    echo COMPILER FAILS ON $SRC, SEE __error.txt
+    exit 1
+  fi
+  rm __error.txt
+  if [ ! -e $CPP ]; then
+    echo COMPILATION FAILED
+    exit 1
+  fi
+
+  if [ "$SRC" != "Library.sref" ]; then
+    $CPPLINE $TEST_CPP_FLAGS -o$TARGET $CPP Library.cpp ../../srlib/refalrts.cpp
+    if [ $? -gt 0 ]; then
+      echo COMPILATION FAILED
+      exit 1
+    fi
+  fi
+}
+
+simple_tests() {
+  MODE=$1
+  shift
+
+  while [ -n "$1" ]; do
+    simple_test_$MODE $1
+    shift
+  done
+}
+
+simple_test_ok() {
+  if [ -e $1 ]; then
+    echo Pass simple OK test $1...
+    run_exe $1
+    cleanup $1
+  fi
+}
+
+simple_test_fail() {
+  if [ -e $1 ]; then
+    echo Pass simple FAIL test $1...
+    if ./$1; then
+      echo TEST NOT EXPECTATIVE FAILED, SEE __dump.txt
+      exit 1
+    fi
+    echo "Ok! This failure was normal and expected"
+    cleanup $1
+  fi
+}
+
+run_exe() {
+  ./$1 > __out.txt
+  if [ $? -gt 0 ]; then
+    echo TEST FAILED, SEE __dump.txt
+    exit 1
+  fi
+}
+
+cleanup() {
+  rm -f $1 $1.cpp __dump.txt __out.txt __written_file.txt
+}
+
+compare() {
+  cmp "$1" "$2"
+  if [ $? -gt 0 ]; then
+    echo FILES $1 and $2 is differ:
+    diff -u "$1" "$2"
+    exit 1
+  fi
+}
+
+if [ -z "$1" ]; then
+  run_all_tests *.sref
+else
+  run_all_tests $1
+fi
