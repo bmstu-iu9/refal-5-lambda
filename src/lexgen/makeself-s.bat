3deb5a0c04c43fc8a2535156f68c548fca13f04b
Elena Burlova <elenaburlova93@gmail.com>
2016-11-14 20:53:48 +0300

    Заготовка нового генератора лексики (#50)

--- src/lexgen/makeself-s.bat
+++ src/lexgen/makeself-s.bat
@@ -1,3 +1,4 @@
 @echo off
 ..\..\distrib\bin\lexgen %LEXGEN_FLAGS% DFA-Lexer.sref
+..\..\distrib\bin\lexgen %LEXGEN_FLAGS% Flex-Lexer.sref
 call ..\make lexgen lexgen LexGen -DDONT_PRINT_STATISTICS ..\..\distrib
