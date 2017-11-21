cbc823714c97d4800963597878de6fa28f5bd7e9
Маздайщик <a.v.konovalov87@mail.ru>
2017-11-21 14:59:31 +0300

    FIXED: исправлено предупреждение компилятора g++
    Обнаружена и исправлена совершенно «тихая» ошибка. При генерации команды
    (#CmdVar #AlgTerm s.Num 's' …) в качестве целевого параметра использовался
    s.ContextOffset, который, однако, не инкрементировался. Также, в качестве
    смещения переменной использовался не s.ContextOffset, а s.Num (смещение
    терма, который анализируется). Поэтому аргумент команды s.ContextOffset
    не использовался. При этом, в соответствующей команде рантайма, refalrts::
    svar_term, данный параметр игнорировался (для единообразия с соседними
    командами он есть).
    
    При компиляции в нативный код в редких случаях оказывалось, что
    подставляемый на это место s.ContextOffset (в виде, например, context[14])
    оказывался равен размеру массива (объявленному, refalrts::Iter context[14]).
    Это обнаруживал бдительный g++ с флагами -Wall и -O3.

--- src/srlib/refalrts.cpp
+++ src/srlib/refalrts.cpp
@@ -551,10 +551,7 @@ void refalrts::call_pointers(
 bool refalrts::svar_term(
   refalrts::Iter /* svar */, refalrts::Iter pos
 ) {
-  if (! is_open_bracket(pos)) {
-    return true;
-  }
-  return false;
+  return ! is_open_bracket(pos);
 }
 
 bool refalrts::svar_left(
