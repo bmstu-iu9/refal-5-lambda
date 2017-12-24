f154ac8b073612c8bb97b118c7f44111829089b0
Маздайщик <a.v.konovalov87@mail.ru>
2017-12-24 16:01:42 +0300

    Сборщик инсталлятора собирает и архив с исходниками

--- make-setup/make-setup.bat
+++ make-setup/make-setup.bat
@@ -7,10 +7,51 @@ goto :EOF
 setlocal
   for /F %%v in ('git describe --dirty') do set VERSION=%%v
 
-  set SETUP=_setup-%VERSION%
+  if not exist _setup\nul mkdir _setup
+  if not exist _tmp\nul mkdir _tmp
 
-  if not exist %SETUP%\nul mkdir %SETUP%
-  pushd %SETUP%
+  call :MAKE_SRC
+  call :MAKE_WINSETUP || exit /b 1
+endlocal
+goto :EOF
+
+:MAKE_SRC
+setlocal
+  pushd ..\distrib
+  call clear.bat
+  popd
+
+  set SETUP=%SETUP%-src
+
+  pushd _tmp
+  for %%f in (..\..\distrib\*) do (
+    if not {%%f}=={.gitignore} (
+      if not {%%f}=={c-plus-plus.conf.bat} (
+        copy %%f %%~nxf
+      )
+    )
+  )
+  for /d %%d in (..\..\distrib\*) do (
+    xcopy /e /i /y %%d %%~nxd
+  )
+
+  set ARC=..\_setup\sources-%VERSION%.zip
+  if not exist %ARC% erase %ARC%
+  start /w winrar m -r -t -afzip %ARC% *
+  popd
+endlocal
+goto :EOF
+
+:MAKE_WINSETUP
+setlocal
+  pushd ..\distrib
+  call clear.bat
+  call bootstrap.bat || exit /b 1
+  popd
+
+  set SETUP=%SETUP%-exe
+
+  pushd _tmp
 
   xcopy /e /i /y ..\..\distrib\bin bin
   xcopy /e /i /y ..\..\distrib\srlib srlib
@@ -22,14 +63,15 @@ setlocal
   copy scripts\c-plus-plus.conf.bat.template c-plus-plus.conf.bat
   copy ..\uninstall.vbs .
   call :MAKERAR
+  popd
 endlocal
 goto :EOF
 
 :MAKERAR
 setlocal
   set FLAGS=m -r -t -sfx -av -k -z..\winrar-comment.txt
-  set ARC=setup-%VERSION%.exe
-  erase %ARC%
+  set ARC=..\_setup\setup-%VERSION%.exe
+  if exist %ARC% erase %ARC%
   start /w winrar %FLAGS% %ARC% *
 endlocal
 goto :EOF
