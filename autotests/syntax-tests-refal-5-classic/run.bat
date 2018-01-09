55eb03fd301c5b380aab2e809427c4682b82a93b
Маздайщик <a.v.konovalov87@mail.ru>
2018-01-09 21:21:17 +0300

    Автотесты синтаксиса для классического РЕФАЛа-5

---
+++ autotests/syntax-tests-refal-5-classic/run.bat
@@ -0,0 +1,36 @@
+@echo off
+
+for %%s in (tests\negative\*.ref) do (
+  echo Passing %%s...
+  ..\..\bin\srefc-core --classic --grammar-check %%s >__out.txt 2>__error.txt
+  if errorlevel 100 (
+    type __out.txt
+    echo COMPILER FAILS, see __error.txt
+    exit /b 1
+  )
+
+  if not errorlevel 1 (
+    type __out.txt
+    echo COMPILER SUCCESSED, BUT EXPECTED SYNTAX ERRORS
+    exit /b 1
+  )
+)
+
+for %%s in (tests\positive\*.ref) do (
+  echo Passing %%s...
+  ..\..\bin\srefc-core --classic --grammar-check %%s >__out.txt 2>__error.txt
+  if errorlevel 100 (
+    type __out.txt
+    echo COMPILER FAILS, see __error.txt
+    exit /b 1
+  )
+
+  if errorlevel 1 (
+    type __out.txt
+    echo FOUND UNEXPECTED SYNTAX ERRORS ON POSITIVE TEST
+    exit /b 1
+  )
+)
+
+if exist __out.txt erase __out.txt
+if exist __error.txt erase __error.txt
---
+++ autotests/syntax-tests-refal-5-classic/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+for s in tests/negative/*.ref; do
+  echo Passing $s...
+  ../../bin/srefc-core --classic --grammar-check $s >__out.txt 2>__error.txt
+  RET=$?
+  if [ $RET -ge 100 ]; then
+    cat __out.txt
+    echo COMPILER FAILS, see __error.txt
+    exit 1
+  fi
+
+  if [ $RET -lt 1 ]; then
+    cat __out.txt
+    echo COMPILER SUCCESSED, BUT EXPECTED SYNTAX ERRORS
+    exit 1
+  fi
+done
+
+for s in tests/positive/*.ref; do
+  echo Passing $s...
+  ../../bin/srefc-core --classic --grammar-check $s >__out.txt 2>__error.txt
+  RET=$?
+  if [ $RET -ge 100 ]; then
+    cat __out.txt
+    echo COMPILER FAILS, see __error.txt
+    exit 1
+  fi
+
+  if [ $RET -ge 1 ]; then
+    cat __out.txt
+    echo FOUND UNEXPECTED SYNTAX ERRORS ON POSITIVE TEST
+    exit 1
+  fi
+done
+
+rm -f __out.txt __error.txt
