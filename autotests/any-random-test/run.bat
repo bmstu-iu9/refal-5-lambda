2fe7d0818f8b17dee054d7f1720d38407f1bcd20
Маздайщик <a.v.konovalov87@mail.ru>
2018-01-15 00:16:24 +0300

    Добавлен автотест на случайные (отрицательные) тесты

---
+++ autotests/any-random-test/run.bat
@@ -0,0 +1,58 @@
+@echo off
+set LOOPS=10
+if not "{%1}"=="{}" set LOOPS=%1
+
+call :MAIN
+goto :EOF
+
+:MAIN
+setlocal
+  call :REGRESSION || exit /b 1
+  call :NEW_TESTS
+endlocal
+goto :EOF
+
+:REGRESSION
+setlocal
+  for %%r in (saved-test-*.ref) do (
+    call :RUN_TEST "%%r"
+
+    if errorlevel 1 exit /b 1
+    erase *.rasl *.cpp *.out* *.err*
+  )
+endlocal
+goto :EOF
+
+:NEW_TESTS
+setlocal
+  set NOW=%DATE%_%TIME%
+  echo start (%NOW%, x%LOOPS%) %TIME%>>time.txt
+  set NOW=%NOW::=-%
+  set NOW=%NOW: =_%
+  ..\..\bin\nemytykh-random-program-generator.exe %LOOPS% _%NOW% /comments
+  echo gen (%NOW%) %TIME%>>time.txt
+  for %%r in (test-*.ref) do (
+    call :RUN_TEST "%%r"
+
+    if not errorlevel 1 (
+      erase "%%~nr.*"
+    )
+  )
+  echo stop (%NOW%) %TIME%>>time.txt
+endlocal
+goto :EOF
+
+:RUN_TEST
+setlocal
+  set FILE="%~n1"
+
+  echo Passing %FILE%:
+  ..\..\bin\srefc-core --classic -C %FILE% --prelude=prelude-for-test ^
+    2>%FILE%.err >%FILE%.out
+  if errorlevel 100 (
+    echo ...COMPILER FAILS ON %FILE%, see %FILE%.err
+    exit /b 1
+  )
+  if exist %FILE%.rasl erase %FILE%.rasl
+endlocal
+exit /b 0
---
+++ autotests/any-random-test/run.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+source ../../scripts/load-config.sh ../.. || exit 1
+source ../../scripts/platform-specific.sh
+
+LOOPS=10
+if [ -n "$1" ]; then
+  LOOPS="$1"
+fi
+
+main() {
+  regression || return 1
+  new_tests
+}
+
+regression() {
+  for r in saved-test-*.ref; do
+    if run_test "$r"; then
+      rm -f *.rasl *.cpp *.out* *.err*
+    else
+      return 1
+    fi
+  done
+}
+
+new_tests() {
+  NOW=$(LC_ALL=C date | sed 's/[ :]/-/g')
+  echo "start ($NOW, x$LOOPS) $(date)" >> time.txt
+
+  ../../bin/nemytykh-random-program-generator $LOOPS _$NOW /comments
+  echo "gen ($NOW) $(date)" >> time.txt
+
+  for r in test-*.ref; do
+    if run_test "$r"; then
+      rm -f ${r%%.ref}.*
+    fi
+  done
+
+  echo "stop ($NOW) $(date)" >> time.txt
+}
+
+run_test() {
+  FILE=${1%%.ref}
+
+  echo "Passing $FILE:"
+  ../../bin/srefc-core --classic -C $FILE --prelude=prelude-for-test \
+    2>$FILE.err >$FILE.out
+
+  if [ $? -gt 99 ]; then
+    echo "...COMPILER FAILS ON $FILE, see $FILE.err"
+    return 1
+  fi
+  rm -f $FILE.rasl
+  return 0
+}
+
+main
