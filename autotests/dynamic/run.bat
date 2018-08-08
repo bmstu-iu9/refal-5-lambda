0012bba8fd67ca8303982b566f98512a3aabe1bc
Маздайщик <a.v.konovalov87@mail.ru>
2018-08-08 11:22:18 +0300

    Подпапки для тестов dynamic (#99)

--- autotests/dynamic/run.bat
+++ autotests/dynamic/run.bat
@@ -1,11 +1,37 @@
 @echo off
-erase *.rasl-module *.rasl *.exe
-call ..\..\bin\srefc -OC -R module.sref
-call ..\..\bin\srmake -X-OC -X--markup-context caller.ref
-caller.exe
-if errorlevel 1 (
-  echo TEST FAILED!
-) else (
-  echo Test OK!
-  erase *.rasl-module *.rasl *.exe
-)
+call :MAIN %* || exit /b 1
+goto :EOF
+
+:MAIN
+setlocal
+  if {%1}=={} (
+    call :RUN_ALL_TESTS_DIR || exit /b 1
+  ) else (
+    for /d %%s in (%*) do call :RUN_TEST_DIR %%s || exit /b 1
+  )
+endlocal
+goto :EOF
+
+:RUN_ALL_TESTS_DIR
+setlocal
+  for /d %%d in (*) do (
+    if exist %%d\run.bat (
+      call :RUN_TEST_DIR %%d || exit /b 1
+    )
+  )
+endlocal
+goto :EOF
+
+:RUN_TEST_DIR
+setlocal
+  echo Passing special test in dir %1...
+  pushd %1
+  call run.bat
+  if errorlevel 1 (
+    echo TEST FAILED
+    popd
+    exit /b 1
+  )
+  popd
+endlocal
+goto :EOF
--- autotests/dynamic/run.sh
+++ autotests/dynamic/run.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-rm -f *.rasl-module *.rasl caller
-../../bin/srefc -OC -R module.sref
-../../bin/srmake -X-OC -X--markup-context caller.ref
-./caller
-if [ $? -gt 0 ]; then
-  echo TEST FAILED!
+run_test_dir() {
+  DIR=$(dirname $1)
+  echo Passing special test in dir $DIR
+  cd $DIR
+  ./run.sh
+  if [ $? -gt 0 ]; then
+    echo TEST FAILED
+    cd ..
+    exit 1
+  fi
+  cd ..
+}
+
+run_all_dir_tests() {
+  for d in */run.sh; do
+    run_test_dir $d
+  done
+}
+
+if [ -z "$1" ]; then
+  run_all_dir_tests
 else
-  echo Test OK!
-  rm -f *.rasl-module *.rasl caller
+  for s in $*; do
+    [ -e $s/run.sh ] && run_test_dir $s/run.sh
+  done
 fi
