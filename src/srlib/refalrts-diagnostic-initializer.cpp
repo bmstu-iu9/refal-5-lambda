50ec68f627c88cb3c3c4b5fc077c651f3a162194
Маздайщик <a.v.konovalov87@mail.ru>
2018-08-21 08:54:28 +0300

    Макросы препроцессора полностью удалены (#171)

--- src/srlib/refalrts-diagnostic-initializer.cpp
+++ src/srlib/refalrts-diagnostic-initializer.cpp
@@ -189,50 +189,6 @@ void load_local_diagnostic_config(
 void init_diagnostic_config(
   refalrts::DiagnosticConfig *config, int *argc, char *argv[]
 ) {
-#ifdef IDENTS_LIMIT
-  config->idents_limit = IDENTS_LIMIT;
-#endif
-
-#ifdef MEMORY_LIMIT
-  config->memory_limit = MEMORY_LIMIT;
-#endif // ifdef MEMORY_LIMIT
-
-#ifdef STEP_LIMIT
-  config->step_limit = STEP_LIMIT;
-#endif // ifdef STEP_LIMIT
-
-#if SHOW_DEBUG
-  config->start_step_trace = SHOW_DEBUG;
-#endif // if SHOW_DEBUG
-
-#ifndef DONT_PRINT_STATISTICS
-  //config->print_statistics = true;
-#endif // ifndef DONT_PRINT_STATISTICS
-
-#ifdef DUMP_FREE_LIST
-  config->dump_free_list = true;
-#endif // ifdef DUMP_FREE_LIST
-
-#ifdef ENABLE_DEBUGGER
-  int debug_arg = refalrts::debugger::find_debugger_flag(*argc, argv);
-  if (debug_arg != -1) {
-    for (int i = debug_arg; i < *argc; ++i) {
-      argv[i] = argv[i + 1];
-    }
-    --*argc;
-    config->debugger_factory = refalrts::debugger::RefalDebugger::create;
-  }
-#else
-  (void) argc;
-  (void) argv;
-#endif // ifdef ENABLE_DEBUGGER
-
-#ifdef DUMP_FILE
-  if (strlen(DUMP_FILE) < sizeof(config->dump_file) - 1) {
-    strcpy(config->dump_file, DUMP_FILE);
-  }
-#endif // ifdef DUMP_FILE
-
   int delta = 0;
   for (int i = 1; i < *argc; ++i) {
     const char config_prefix[] = "++diagnostic+config=";
