818ed4939b44dcc87e3dd1e7ad3bd871dbf8a61f
Маздайщик <a.v.konovalov87@mail.ru>
2019-03-10 15:04:34 +0300

    Пользовательский интерфейс декомпилятора

--- make.bat
+++ make.bat
@@ -3,5 +3,5 @@ refc test.ref
 call srefc -C test.ref
 if exist decompiler.exe erase decompiler.exe
 call srmake -X--markup-context decompiler
-decompiler test.rsl > test.out
-type test.out
+decompiler test.rsl
+type test-decompiled.ref
