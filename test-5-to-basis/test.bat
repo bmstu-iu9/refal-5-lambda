ab4a51edc89ebc77b18a0d02ef39192454e62592
Маздайщик <a.v.konovalov87@mail.ru>
2019-03-11 02:55:06 +0300

    Длинные строчки в файле разбиваются переносами

--- test-5-to-basis/test.bat
+++ test-5-to-basis/test.bat
@@ -1,14 +1,14 @@
 @echo off
 call clear.bat
-for %%r in (*.ref) do refc %%r
 for %%r in (*.ref) do call :TEST %%r
 for %%d in (*.diff) do type %%d
 goto :EOF
 
 :TEST
+  refc %~n1
   ..\decompiler %~n1.rsl
   ren %1 %1.bak
-  refgo Lexer+Main+Parser+Plainer+Tests+Transformer+Utils %~n1-decompiled.ref %1
+  ren %~n1-decompiled.ref %1
   ren %~n1.rsl %~n1.rsl.bak
   refc %1
   fc /b %~n1.rsl %~n1.rsl.bak > %1.diff
