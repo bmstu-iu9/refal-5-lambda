b2992774d09fe2ade4d71332e9526456329fee3a
Маздайщик <a.v.konovalov87@mail.ru>
2019-06-03 12:29:46 +0300

    Обновлена стабильная версия

--- docs/setup.bash
+++ docs/setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 (
-  VERSION=2.3.2
+  VERSION=2.3.3
 
   echo $(date): "*** Installing Refal-5λ $VERSION ***" | cat
   echo $(date): 'Prepare directory ~/.local/share/refal-5-lambda' | cat
