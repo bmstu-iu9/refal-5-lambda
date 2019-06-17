f3828926d94ed7e13d9755972da007d9efa9b4d7
Маздайщик <a.v.konovalov87@mail.ru>
2019-06-17 12:25:46 +0300

    Оптимизация порождения RASL’а при помощи $INLINE и $DRIVE (#128)

--- src/compiler/makeself-s.bat
+++ src/compiler/makeself-s.bat
@@ -1,4 +1,4 @@
 @echo off
 ..\..\distrib\bin\lexgen %LEXGEN_FLAGS% SR-Lexer.sref
-..\..\bin\rasl-constants.exe ../srlib/refalrts-commands.h Opcodes.ref
+..\..\bin\rasl-constants.exe ../srlib/refalrts-commands.h Opcodes.refi
 call ..\make compiler srefc-core srefc "" ..\..\distrib
--- src/compiler/makeself.sh
+++ src/compiler/makeself.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ../../bin/lexgen $LEXGEN_FLAGS SR-Lexer.sref
-../../bin/rasl-constants ../srlib/refalrts-commands.h Opcodes.ref
+../../bin/rasl-constants ../srlib/refalrts-commands.h Opcodes.refi
 source ../make.sh compiler srefc-core srefc
