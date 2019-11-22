1e2b295d2dc941405cfd66d3ecf2f772cb1d77c0
Маздайщик <a.v.konovalov87@mail.ru>
2019-11-22 10:12:59 +0300

    Стилевые правки

--- src/compiler/makeself-s.sh
+++ src/compiler/makeself-s.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-../../distrib/bin/lexgen $LEXGEN_FLAGS SR-Lexer.sref
+../../distrib/bin/lexgen ${LEXGEN_FLAGS} SR-Lexer.sref
 ../../bin/rasl-constants ../lib/refalrts-commands.h Opcodes.refi
 source ../make.sh compiler srefc-core srefc "" ../../distrib
--- src/compiler/makeself.sh
+++ src/compiler/makeself.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-../../bin/lexgen $LEXGEN_FLAGS SR-Lexer.sref
+../../bin/lexgen ${LEXGEN_FLAGS} SR-Lexer.sref
 ../../bin/rasl-constants ../lib/refalrts-commands.h Opcodes.refi
 source ../make.sh compiler srefc-core srefc
