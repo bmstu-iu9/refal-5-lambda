244d7be7343aa8108ca98df699f488b002cfa57c
Маздайщик <a.v.konovalov87@mail.ru>
2020-02-05 16:21:03 +0300

    Предупреждения компилятора GNU C++
    Без атрибута noreturn компилятор g++ с ключом -Wall -O1 ругался примерно
    на десяток функций с refalrts_switch_default_violation() — сообщал
    о возможных неинициализированных переменных. Коверкать код для подавления
    предупреждений менее целесообразно, чем добавление этого атрибута.

--- src/lib/refalrts.h
+++ src/lib/refalrts.h
@@ -379,6 +379,10 @@ const char* arg(VM *vm, unsigned int param);
 void debug_print_expr(VM *vm, void *file, Iter first, Iter last);
 
 
+/* Без этого атрибута GNU C++ выдаёт много предупреждений */
+#if defined(__GNUC__)
+__attribute__((noreturn))
+#endif
 void switch_default_violation_impl(
   const char *filename, int line_no, long bad_switch_value, const char *bad_expr
 );
