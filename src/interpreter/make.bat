20e82538c74dbd161797bf6abe35c63eff0f9f78
Маздайщик <a.v.konovalov87@mail.ru>
2020-04-07 00:22:54 +0300

    Уточнение подготовки файлов для bootstrap’а интерпретатора (#48)

--- src/interpreter/make.bat
+++ src/interpreter/make.bat
@@ -2,4 +2,7 @@
 setlocal
   set SCRIPT_FLAGS=%SCRIPT_FLAGS% --debug --scratch
   call ..\make interpreter rlgo main
+
+  copy ..\..\lib\scratch-rt\debug\*.rasl ..\..\build\interpreter >NUL
+  copy ..\..\lib\scratch-rt\debug\*.cpp ..\..\build\interpreter >NUL
 endlocal
--- src/interpreter/make.sh
+++ src/interpreter/make.sh
@@ -2,4 +2,6 @@
 (
   SCRIPT_FLAGS="$SCRIPT_FLAGS --debug --scratch"
   source ../make.sh interpreter rlgo main
+
+  cp ../../lib/scratch-rt/debug/*.{rasl,cpp} ../../build/interpreter
 )
