3e24d4e0e20e6063a433186988a939cf179fb126
Маздайщик <a.v.konovalov87@mail.ru>
2020-05-14 12:29:44 +0300

    Автотесты для поиска дубликатов entry-функций (#265)

---
+++ autotests/dynamic/D-multiple-entry/run.bat
@@ -0,0 +1,11 @@
+@echo off
+erase *.rasl-module *.rasl *.exe
+..\..\..\bin\rlc-core -R -o target.rasl-module --keep-rasls unit1.ref unit2.ref
+if exist target.rasl-module (
+  echo TEST FAILED: linker error is expected!
+  exit /b 1
+) else (
+  echo Test OK: linker found expected errors!
+  erase *.rasl-module *.rasl *.exe
+  exit /b 0
+)
---
+++ autotests/dynamic/D-multiple-entry/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -f *.rasl-module *.rasl caller
+../../../bin/rlc --keep-rasls -OC -R module.sref -r foo
+../../../bin/rlc-core -R -o target.rasl-module --keep-rasls unit1.ref unit2.ref
+if [[ -e target.rasl-module ]]; then
+  echo TEST FAILED: linker error is expected!
+  exit 1
+else
+  echo Test OK: linker found expected errors!
+  rm -f *.rasl-module *.rasl caller
+  exit 0
+fi
