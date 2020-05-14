f43c0198dfa8f344ccbbb4a2a65367e3bffb19a3
Маздайщик <a.v.konovalov87@mail.ru>
2020-05-14 15:35:13 +0300

    FIXED: автотест на загрузку файла с двумя одноимёнными функциями (#265)
    Данный автотест работать перестал, но проходил. Файл не создавался,
    однако командные оболочки (cmd.exe и Bash) при запуске отсутствующего
    файла устанавливали ненулевой код возврата, что удовлетворяло автотест.

--- autotests/dynamic/9-redeclaration-fail/run.bat
+++ autotests/dynamic/9-redeclaration-fail/run.bat
@@ -1,6 +1,15 @@
 @echo off
 erase *.rasl-module *.rasl *.exe
-call ..\..\..\bin\rlmake --keep-rasls -X-OC -X--markup-context caller.ref
+
+rem Теперь компилятор проверяет дубликаты entry-функций при создании,
+rem поэтому исполнимый файл с двумя одноимёнными entry-функциями
+rem нельзя создать командой
+rem call ..\..\..\bin\rlmake --keep-rasls -X-OC -X--markup-context caller.ref
+
+call ..\..\..\bin\rlc --no-sources -o prefix.exe
+call ..\..\..\bin\rlc -C -OC --markup-context caller.ref
+copy /b prefix.exe+caller.rasl caller.exe
+
 caller.exe
 if not errorlevel 1 (
   echo TEST FAILED! Program must be fail!
--- autotests/dynamic/9-redeclaration-fail/run.sh
+++ autotests/dynamic/9-redeclaration-fail/run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 rm -f *.rasl-module *.rasl caller
-../../../bin/rlmake --keep-rasls -X-OC -X--markup-context caller.ref
+
+# Теперь компилятор проверяет дубликаты entry-функций при создании,
+# поэтому исполнимый файл с двумя одноимёнными entry-функциями
+# нельзя создать командой
+#
+# ../../../bin/rlmake --keep-rasls -X-OC -X--markup-context caller.ref
+#
+# Поэтому создаём ошибочный файл искусственно
+
+../../../bin/rlc --no-sources -o caller
+../../../bin/rlc -C -OC --markup-context caller.ref
+cat caller.rasl >> ./caller
+
 ./caller
 if [[ $? -eq 0 ]]; then
   echo TEST FAILED! Program must be fail!
