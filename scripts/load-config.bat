568957b993ca02cb8a2d3b9a6f2d9fc9bc1c73a3
Маздайщик <a.v.konovalov87@mail.ru>
2020-05-20 08:50:19 +0300

    FIXED: путь к %DEFFILE% задаётся извне c-plus-plus.conf.bat (#279)

--- scripts/load-config.bat
+++ scripts/load-config.bat
@@ -2,6 +2,12 @@ if not "%CPPLINEE%" == "" exit /b 0
 
 set CONFIG_FILE="%~dp0..\c-plus-plus.conf.bat"
 if not exist %CONFIG_FILE% copy "%~dp0c-plus-plus.conf.bat.template" %CONFIG_FILE% >NUL
+
+set DEFFILE=%~dp0..\lib\scratch-rt\platform-Windows\lib\refalrts-main.def
+if not exist "%DEFFILE%" (
+  set DEFFILE=%~dp0..\src\lib\platform-Windows\lib\refalrts-main.def
+)
+
 call %CONFIG_FILE%
 
 if not "%CPPLINEE%" == "" exit /b 0
