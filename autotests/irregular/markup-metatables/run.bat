e3d08e9a54decdb2038198f51d07bc5e7b3b9429
Маздайщик <a.v.konovalov87@mail.ru>
2020-10-26 00:13:03 +0300

    Добавлена поддержка запуска нескольких тестов (#325)
    Коммит лучше смотреть без учёта пробельных символов (например, дописав
    ?w=1 в строке адреса страницы в GitHub).

--- autotests/irregular/markup-metatables/run.bat
+++ autotests/irregular/markup-metatables/run.bat
@@ -1,28 +1,33 @@
 @echo off
 
 call :CLEANUP
-
-call ..\..\..\bin\rlc ++diagnostic+config=rlc-core.ini ^
-  test.ref -OADiS --log=__test.log
-if errorlevel 1 (
-  echo COMPILER FAILED!
-  exit /b 1
-)
-
-if not exist test.exe (
-  echo COMPILATION ERRORS!
-  exit /b 1
-)
-
-test.exe ++diagnostic+config=test.ini
-if errorlevel 1 (
-  echo TEST FAILED!
-  exit /b 1
-)
-
-echo Test OK!
-call :CLEANUP
+call :RUN_TEST test-Rec-Mu || exit /b 0
 exit /b 0
+
+:RUN_TEST
+  echo Run %1...
+  call ..\..\..\bin\rlc ++diagnostic+config=rlc-core.ini ^
+    %1.ref -o test.exe -OADiS --log=__test.log
+  if errorlevel 1 (
+    echo COMPILER FAILED!
+    exit /b 1
+  )
+
+  if not exist test.exe (
+    echo COMPILATION ERRORS!
+    exit /b 1
+  )
+
+  test.exe ++diagnostic+config=test.ini
+  if errorlevel 1 (
+    echo TEST FAILED!
+    exit /b 1
+  )
+
+  echo Test OK!
+  call :CLEANUP
+  exit /b 0
+goto :EOF
 
 :CLEANUP
   for %%f in (*.rasl-module *.rasl *.exe *.log *.dump) do (
--- autotests/irregular/markup-metatables/run.sh
+++ autotests/irregular/markup-metatables/run.sh
@@ -4,25 +4,28 @@ cleanup() {
   rm -f *.rasl-module *.rasl test *.log
 }
 
+run_test() {
+  echo Run $1...
+  ../../../bin/rlc ++diagnostic+config=rlc-core.ini \
+    $1.ref -o test -OADiS --log=__test.log
+  if [[ $? -gt 0 ]]; then
+    echo COMPILER FAILED!
+    exit 1
+  fi
+
+  if [[ ! -e ./test ]]; then
+    echo COMPILATION ERRORS!
+    exit 1
+  fi
+
+  ./test ++diagnostic+config=test.ini
+  if [[ $? -gt 0 ]]; then
+    echo TEST FAILED!
+    exit 1
+  fi
+
+  echo Test OK!
+}
+
 cleanup
-
-../../../bin/rlc ++diagnostic+config=rlc-core.ini \
-  test.ref -OADiS --log=__test.log
-if [[ $? -gt 0 ]]; then
-  echo COMPILER FAILED!
-  exit 1
-fi
-
-if [[ ! -e ./test ]]; then
-  echo COMPILATION ERRORS!
-  exit 1
-fi
-
-./test ++diagnostic+config=test.ini
-if [[ $? -gt 0 ]]; then
-  echo TEST FAILED!
-  exit 1
-fi
-
-echo Test OK!
-cleanup
+run_test test-Rec-Mu
