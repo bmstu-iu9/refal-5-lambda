e998fb991fcb79638295cd8d9641cda9c1449a2f
Маздайщик <a.v.konovalov87@mail.ru>
2020-11-09 10:32:17 +0300

    FIXED: при глобальной оптимизации __INIT и __FINAL вызываются (#298)

---
+++ autotests/dynamic/F-global-init-final/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+check_exist() {
+  if [[ -e $1 ]]; then
+    echo File $1 exists...
+    rm $1
+  else
+    echo TEST FAILED: file $1 does not exist!
+    exit 1
+  fi
+}
+
+rm -f *.rasl-module *.rasl caller
+
+../../../bin/rlc --keep-rasls main.ref lib1.ref lib2.ref --log=log.txt
+./main
+if [[ $? -gt 0 ]]; then
+  echo TEST FAILED!
+  exit 1
+else
+  check_exist 1-init.ok
+  check_exist 2-go.ok
+  check_exist 3-final.ok
+
+  echo Test OK!
+  rm -f *.rasl-module *.rasl caller log.txt
+fi
+
+../../../bin/rlc --keep-rasls -OG main.ref lib1.ref lib2.ref --log=log.txt
+./main
+if [[ $? -gt 0 ]]; then
+  echo TEST FAILED!
+  exit 1
+else
+  check_exist 1-init.ok
+  check_exist 2-go.ok
+  check_exist 3-final.ok
+
+  echo Test OK!
+  rm -f *.rasl-module *.rasl caller *-locals.lst log.txt
+fi
