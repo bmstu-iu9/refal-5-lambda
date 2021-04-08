265ecfe40286227724140278437755cebb987de5
Маздайщик <a.v.konovalov87@mail.ru>
2021-04-08 10:35:58 +0300

    Удалён неиспользуемый параметр в scripts/load-config.sh (#323)
    Параметр был добавлен в коммите 06222c0414 по ошибке, сначала его
    предполагалось как-то использовать, а потом забыл удалить.

--- scripts/load-config.sh
+++ scripts/load-config.sh
@@ -2,12 +2,12 @@
 
 CONFIG_FILE=$1/c-plus-plus.conf.sh
 [ -e "$CONFIG_FILE" ] || cp $1/scripts/c-plus-plus.conf.sh.template "$CONFIG_FILE"
-. $CONFIG_FILE "$1"
+. $CONFIG_FILE
 
 [ -n "$CPPLINEE" ] && return 0
 
 if [ -e $1/../c-plus-plus.conf.sh ]; then
-  . $1/../c-plus-plus.conf.sh "$1"
+  . $1/../c-plus-plus.conf.sh
 fi
 
 [ -n "$CPPLINEE" ] && return 0
